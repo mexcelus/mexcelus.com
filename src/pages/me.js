62469df8ff55062685742bd3d4d6ea079434ca91
Guillermo Villalta <mexcelus@gmail.com>
2019-08-16 18:47:03 -0600

    adjusted graphql query

---
+++ src/pages/me.js
@@ -0,0 +1,44 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const MePage = ({ data }) => (
+    <Layout>
+        <div>
+            <br></br>
+            <br></br>
+            <br></br>
+        <h1>Resume</h1>
+        {data.allMarkdownRemark.edges.map(post => (
+            <div hey={post.node.id}>
+                <h3>
+                    {post.node.frontmatter.title}
+                </h3>
+                <div dangerouslySetInnerHTML={{ __html: post.node.html }} />
+                <br />
+                <br />
+            </div>
+        ))}
+        </div>
+    </Layout>
+)
+
+export const pageQuery = graphql`
+    query MeIndexQuery {
+        allMarkdownRemark (filter: {frontmatter: {slug: {eq: "resume"}}}){
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        path
+                        slug
+                        template
+                    }
+                    html
+                }
+            }
+        }
+    }
+`
+export default MePage
