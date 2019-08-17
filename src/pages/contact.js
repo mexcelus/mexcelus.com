2ebfec9dce0a62aa46cd50f913bdb80c71468274
Guillermo Villalta <guillermovillalta@Guillermos-iMac.local>
2019-08-17 12:06:41 -0600

    render page

---
+++ src/pages/contact.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Footer from "../components/footer"
+import SubForm from "../components/subform"
+
+// import "../components/twitter"
+
+const ContactPage = () => (
+  <Layout>
+    <br></br>
+    <br></br>
+    <div className="main-container">
+    </div>
+    <SubForm />
+    <Footer />
+  </Layout>
+)
+
+export default ContactPage
