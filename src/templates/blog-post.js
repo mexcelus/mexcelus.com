ecfaced9e12cd41238c8ce84b05deda212bbaf23
Guillermo Villalta <mexcelus@gmail.com>
2019-08-27 22:28:20 -0600

    added layout + className

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from 'react'
 import Link from 'gatsby-link'
-import { graphql } from 'gatsby';
+import { graphql } from 'gatsby'
+
+import Layout from "../components/layout"
 
 export default function Template({data}) {
     const post = data.markdownRemark
 
     return(
-        <div>
+        <Layout>
+        <div className="blog-post">
             <Link to="/blog">Go back</Link>
             <hr />
             <h1>{post.frontmatter.tittle}</h1>
             <h4>Posted by {post.frontmatter.author} on {post.frontmatter.date}</h4>
             <div dangerouslySetInnerHTML={{__html: post.html}}/>
         </div>
+        </Layout>
     )
 }
 
