6b015da1546f1cd6a1f216e09ec733efe52ae6c9
Guillermo Villalta <mexcelus@gmail.com>
2019-08-30 15:21:07 -0600

    removed social media

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ import Layout from "../components/layout"
 import Footer from "../components/footer"
 import SubForm from "../components/subform"
 import Image from "../components/image"
-import Tweet from "../components/twitter-img"
-import Octocat from "../components/github-img"
-import LinkedIn from "../components/linkedin-img"
+// import Tweet from "../components/twitter-img"
+// import Octocat from "../components/github-img"
+// import LinkedIn from "../components/linkedin-img"
 import TreeHouse from "../components/treehouse"
 import TreeMower from "../components/treemower"
 
@@ -26,27 +26,16 @@ const IndexPage = () => (
     <br></br>
     <div className="my-Name">
       <div className="my-Input my-Name">
-        <div style={{width: '350px'}}>
+        <div className="my-picture">
           <Image  /> 
         </div>
-        <div>
+        <div className="my-info">
           <h1>
             Guillermo V.
           </h1>
           <p>
             I'm a software developer working with modern Javascript trying to record my journey into software development. Based on SLC, Utah. I enjoy designing and building new stuff, dissecting complex integrations and contributing to open source. <span className="garden">Welcome to my digital garden.</span>
           </p>
-          <div className='my-Name' style={{ paddingLeft: '20px' }}>
-            <div style={{ width: '50px', height: '50px' }}>
-              <a href="https://twitter.com/mexcelus"><Tweet /></a>
-            </div>
-            <div style={{ width: '45px', height: '45px' }}>
-              <a href="https://github.com/mexcelus"><Octocat /></a>
-            </div>
-            <div style={{ width: '50px', height: '50px' }}>
-              <a href="https://www.linkedin.com/in/guillermo-villalta-024b3693/"><LinkedIn /></a>
-            </div>
-          </div>
         </div>
       </div>
     </div>
